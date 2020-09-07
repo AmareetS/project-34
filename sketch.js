a71cf46d544523359d02312d7cf468793883b678
AmareetS <68671075+AmareetS@users.noreply.github.com>
2020-09-06 21:59:32 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var database;
+var dog, dog_img, happydog_img
+var foodS, foodstock;
+
+
+function preload() {
+  dog_img = loadImage("dogImg.png");
+  happydog_img = loadImage("dogImg1.png");
+}
+
+
+function setup() {
+  createCanvas(500, 500);
+
+  dog = createSprite(250, 350);
+  dog.addImage("normal", dog_img);
+  dog.scale = 0.3;
+
+  database = firebase.database();
+
+  foodstock = database.ref("Food");
+  foodstock.on("value", readStock);
+}
+
+
+function draw() {
+  background(46, 139, 87);
+
+  if (keyWentDown(UP_ARROW)) {
+    writeStock();
+    dog.addImage("happy", happydog_img);
+    dog.changeImage("happy");
+  }
+
+  drawSprites();
+  textSize(20);
+  stroke(255);
+  textAlign(CENTER, CENTER);
+  text("Food Remaining: " + foodS, 250, 100);
+}
+
+
+function readStock(data) {
+  foodS = data.val();
+}
+
+
+function writeStock() {
+
+  if (foodS < 0) {
+    foodS = 0
+  }
+
+  database.ref("/").update({
+    "Food": foodS--
+  })
+}
